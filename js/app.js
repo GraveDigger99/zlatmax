cf705a3de93f77f18e2b95c9c861ad6361ec5e17
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2023-01-18 12:43:31 +0400

    add:flies

--- js/app.js
+++ js/app.js
@@ -9183,7 +9183,6 @@
                     observeParents: true,
                     slidesPerView: 4,
                     spaceBetween: 16,
-                    speed: 800,
                     breakpoints: {
                         320: {
                             slidesPerView: 2,
@@ -9222,6 +9221,32 @@
             use_native: true
         });
         let addWindowScrollEvent = false;
+        function headerScroll() {
+            addWindowScrollEvent = true;
+            const header = document.querySelector("header.header");
+            const headerShow = header.hasAttribute("data-scroll-show");
+            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+            const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
+            let scrollDirection = 0;
+            let timer;
+            document.addEventListener("windowScroll", (function(e) {
+                const scrollTop = window.scrollY;
+                clearTimeout(timer);
+                if (scrollTop >= startPoint) {
+                    !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
+                    if (headerShow) {
+                        if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        timer = setTimeout((() => {
+                            !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        }), headerShowTimer);
+                    }
+                } else {
+                    header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
+                    if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
+                }
+                scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+            }));
+        }
         setTimeout((() => {
             if (addWindowScrollEvent) {
                 let windowScroll = new Event("windowScroll");
@@ -9368,5 +9393,6 @@
         formSubmit();
         formQuantity();
         formRating();
+        headerScroll();
     })();
 })();
